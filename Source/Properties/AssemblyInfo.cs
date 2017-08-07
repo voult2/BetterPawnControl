4a753dc49ad8f040d9d4afc22fb9ffe3f9aeff6c
VouLT <alexandreCbrito@gmail.com>
2017-08-07 19:01:26 +0100

    Added french language support

--- Source/Properties/AssemblyInfo.cs
+++ Source/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.6.4.0")]
+[assembly: AssemblyVersion("1.6.5.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
