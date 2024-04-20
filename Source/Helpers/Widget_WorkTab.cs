7e307c815c8d8155cfd2fc033459fddd605a4229
Oleg Bugrov <git@lightwave.me>
2024-04-20 14:46:29 +0400

    Improve performance of Work Tab set priorities. (#43)

--- Source/Helpers/Widget_WorkTab.cs
+++ Source/Helpers/Widget_WorkTab.cs
@@ -18,6 +18,15 @@ namespace BetterPawnControl
         private static MethodInfo getPriorities;
         private static MethodInfo setPriority;
 
+        private static Type priorityManager;
+        private static MethodInfo getPriorityTrackerByPawn;
+
+        private static GameComponent gcPriorityManager;
+
+        private static Type priorityTracker;
+        private static MethodInfo invalidateCache;
+        private static MethodInfo onChange;
+
         static Widget_WorkTab()
         {
             var isModActive = LoadedModManager.RunningMods.Any(mod => mod.Name == "Work Tab");
@@ -31,9 +40,27 @@ namespace BetterPawnControl
                 setPriority = AccessTools.Method(pawnExtensions, "SetPriority", new Type[] { typeof(Pawn), typeof(WorkGiverDef), typeof(int), typeof(int), typeof(bool) });
             }
 
+            priorityManager = AccessTools.TypeByName("WorkTab.PriorityManager");
+            if (priorityManager != null)
+            {
+                getPriorityTrackerByPawn = AccessTools.PropertyGetter(priorityManager, "Item");
+            }
+
+            priorityTracker = AccessTools.TypeByName("WorkTab.PriorityTracker");
+            if (priorityTracker != null)
+            {
+                invalidateCache = AccessTools.Method(priorityTracker, "InvalidateCache", new Type[] { typeof(WorkGiverDef), typeof(bool) });
+                onChange = AccessTools.Method(priorityTracker, "OnChange", null);
+            }
+
             init = pawnExtensions != null
                 && getPriorities != null
-                && setPriority != null;
+                && setPriority != null
+                && priorityManager != null
+                && getPriorityTrackerByPawn != null
+                && priorityTracker != null
+                && invalidateCache != null
+                && onChange != null;
 
             if (init)
                 Log.Message("[BPC] Work Tab functionality integrated");
@@ -48,9 +75,26 @@ namespace BetterPawnControl
                     sb.AppendLine(" - Method WorkTab.Pawn_Extensions:GetPriorities(Pawn, WorkGiverDef) is not found.");
                 if (setPriority == null)
                     sb.AppendLine(" - Method WorkTab.Pawn_Extensions:SetPriority(Pawn, WorkGiverDef, int, int, bool) is not found.");
-                
+
+                if (priorityManager == null)
+                    sb.AppendLine(" - Type WorkTab.PriorityManager is not found.");
+                if (getPriorityTrackerByPawn == null)
+                    sb.AppendLine(" - Method WorkTab.PriorityManager:get_Item(Pawn) is not found.");
+
+                if (priorityTracker == null)
+                    sb.AppendLine(" - Type WorkTab.PriorityTracker is not found.");
+                if (invalidateCache == null)
+                    sb.AppendLine(" - Method WorkTab.PriorityTracker:InvalidateCache(WorkGiverDef, bool) is not found.");
+                if (onChange == null)
+                    sb.AppendLine(" - Method WorkTab.PriorityTracker:OnChange() is not found.");
+
                 Log.Error(sb.ToString());
             }
+        }
+
+        public static void ClearCache()
+        {
+            gcPriorityManager = null;
         }
 
         public static List<int> GetWorkTabPriorities(Pawn pawn, WorkGiverDef workGiver)
@@ -62,6 +106,11 @@ namespace BetterPawnControl
 
             if (!init)
                 return null;
+
+            if (gcPriorityManager == null)
+            {
+                gcPriorityManager = Current.Game?.GetComponent(priorityManager);
+            }
 
             var array = (int[]) getPriorities.Invoke(null, new object[] { pawn, workGiver });
             var result = new List<int>(array);
@@ -83,7 +132,22 @@ namespace BetterPawnControl
 
             for (int hour = 0; hour < hours.Count; hour++)
             {
-                setPriority.Invoke(null, new object[] { pawn, workGiver, hours[hour], hour, true });
+                setPriority.Invoke(null, new object[] { pawn, workGiver, hours[hour], hour, false });
+            }
+
+            if (gcPriorityManager == null)
+            {
+                gcPriorityManager = Current.Game?.GetComponent(priorityManager);
+            }
+
+            if (gcPriorityManager != null)
+            {
+                var pt = getPriorityTrackerByPawn.Invoke(gcPriorityManager, new object[] { pawn });
+                if (pt != null)
+                {
+                    invalidateCache.Invoke(pt, new object[] { workGiver, true });
+                    onChange.Invoke(pt, null);
+                }
             }
         }
     }
