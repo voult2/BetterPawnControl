ebc605f91064fc396fa682ec5fc0179cfc1cd6fd
Angelo Locritani <67960642+angelolocritani@users.noreply.github.com>
2025-01-31 17:19:16 +0100

    Make the default set by this mod visible in vanilla (#63)
    Make the default set by this mod visible in vanilla dialogs (* and (default) text)

---
+++ Source/Patches/Dialog_ManageApparelPolicies_GetDefaultPolicy.cs
@@ -0,0 +1,21 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace BetterPawnControl.Patches
+{
+    [HarmonyPatch(typeof(Dialog_ManageApparelPolicies), "GetDefaultPolicy")]
+    static class Dialog_ManageApparelPolicies_GetDefaultPolicy
+    {
+        static void Postfix(ref ApparelPolicy __result)
+        {
+            __result = AssignManager.DefaultOutfit;
+        }
+        
+    }
+}
---
+++ Source/Patches/Dialog_ManageReadingPolicies_GetDefaultPolicy.cs
@@ -0,0 +1,21 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace BetterPawnControl.Patches
+{
+    [HarmonyPatch(typeof(Dialog_ManageReadingPolicies), "GetDefaultPolicy")]
+    static class Dialog_ManageReadingPolicies_GetDefaultPolicy
+    {
+        static void Postfix(ref ReadingPolicy __result)
+        {
+            __result = AssignManager.DefaultReadingPolicy;
+        }
+
+    }
+}
