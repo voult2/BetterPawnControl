e0f691e3f392436d6b1978899cd9e0bf0541d653
Alexandre Brito <alexandre.brito@outlook.pt>
2025-03-16 12:47:09 +0000

    - Fixed emergency button not working for Mechs if Ideology was disabled

--- Source/Managers/AlertManager.cs
+++ Source/Managers/AlertManager.cs
@@ -78,7 +78,7 @@ namespace BetterPawnControl
                     //This means the alertLevelsList is missing a default policy
                     //This can be caused by loading a save from 1.3 on 1.4 (missing the new mech data or other)
                     case Resources.Type.mech:
-                        if (ModsConfig.IdeologyActive) 
+                        if (ModsConfig.BiotechActive) 
                         { 
                             alertLevelsList[level].settings.Add(Resources.Type.mech, MechManager.GetActivePolicy());
                             alertPolicy = MechManager.GetActivePolicy();
@@ -135,7 +135,7 @@ namespace BetterPawnControl
                     alertLevel.settings.SetOrAdd(Resources.Type.assign, AssignManager.GetActivePolicy());
                     alertLevel.settings.SetOrAdd(Resources.Type.animal, AnimalManager.GetActivePolicy());
 
-                    if (ModsConfig.IdeologyActive)
+                    if (ModsConfig.BiotechActive)
                     {
                         alertLevel.settings.SetOrAdd(Resources.Type.mech, MechManager.GetActivePolicy());
                     }
@@ -182,7 +182,7 @@ namespace BetterPawnControl
                             AnimalManager.LoadState(entry.Value);
                             break;
                         case Resources.Type.mech:
-                            if (ModsConfig.IdeologyActive)
+                            if (ModsConfig.BiotechActive)
                             {
                                 MechManager.LoadState(entry.Value);
                             }                                
